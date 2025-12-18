b1ee73872c495deb0f9cfe740482f3f0d0a50c60
novenkov-m <novenckov.mikhail@gmail.com>
2025-12-18 10:25:29 +0300

    Add files via upload
    загрузил класс скина и коллекцию первого оружейного кейса

---
+++ skins.cc
@@ -0,0 +1,56 @@
+#include "skins.h"
+#include <vector>
+
+
+
+std::vector<SkinData> skin_database = {
+{"Lightning Strike","AWP",
+    "The Arms Deal collection",
+    "Covert",
+    0.03, 801.07},
+
+    {"Case Hardened","AK-47",
+    "The Arms Deal collection",
+    "Classified",
+    0.23, 254.79},
+
+    {"Hypnotic","Desert Eagle",
+    "The Arms Deal collection",
+    "Classified",
+    0.04, 246.77},
+    
+    {"Dragon Tatto","Glock-18",
+    "The Arms Deal collection",
+    "Restricted",
+    0.01, 194.80},
+    
+    {"Dark Water","M4A1-S",
+    "The Arms Deal collection",
+    "Restricted",
+    0.13, 121.88},
+    
+    {"Dark Water","USP-S",
+    "The Arms Deal collection",
+    "Restricted",
+    0.20, 116.52},
+
+    {"Dark Water","USP-S",
+    "The Arms Deal collection",
+    "Restricted",
+    0.20, 116.52},
+    
+    {"Wings","AUG",
+    "The Arms Deal collection",
+    "Mil-Spec",
+    0.09, 17.93},
+    
+    {"Skulls","MP-7",
+    "The Arms Deal collection",
+    "Mil-Spec",
+    0.16, 14.36},
+    
+    {"Ultraviolet","SG-553",
+    "The Arms Deal collection",
+    "Mil-Spec",
+    0.51, 13.48}
+};
