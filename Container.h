1e53d6be3553816a4b7c1c1735625fdbe3e7740e
novenkov-m <novenckov.mikhail@gmail.com>
2025-12-21 22:29:09 +0300

    Add files via upload
    New Container -> WeaponCase classes, Item -> Weapon_Skin classes

---
+++ Container.h
@@ -0,0 +1,35 @@
+#ifndef CASE_H
+#define CASE_H
+
+#include <vector>
+#include <memory>      
+#include <random>
+#include <string>
+
+#include "Item.h"
+
+class Container {
+protected:
+    std::string name_;
+    double price_;
+    std::vector<std::unique_ptr<Item>> items_; 
+    std::mt19937 rng_; 
+    virtual std::unique_ptr<Item> selectItem();
+
+public:
+    Container(std::string name, double price); 
+    virtual ~Container() = default;
+
+    virtual std::string getName() ;
+    virtual double getPrice() ;
+    virtual std::unique_ptr<Item> open();
+    virtual void addItem(std::unique_ptr<Item> skin);
+    virtual void displayContents();
+
+    virtual std::unique_ptr<Container> clone() = 0;
+
+    virtual bool operator==(Container& other);
+    virtual Container& operator+=(std::unique_ptr<Item> item);
+};
+
+#endif // CASE_H
