eb981abca8524e9e309c5d54f500209130ab33a3
novenkov-m <novenckov.mikhail@gmail.com>
2025-12-25 18:17:43 +0300

    Update WeaponCase.cc

--- WeaponCase.cc
+++ WeaponCase.cc
@@ -1,47 +1,43 @@
 #include "WeaponCase.h"
+#include "Item.h"
+#include <memory>
 #include <stdexcept>
 #include <random>
 
 WeaponCase::WeaponCase(std::string name, double price)
     : Container(std::move(name), price) {}
-
-std::unique_ptr<Container> WeaponCase::clone()  {
-    auto newCase = std::make_unique<WeaponCase>(name_, price_);
-    for ( auto& skin : items_) {
-        if (auto* wpn = dynamic_cast<Weapon_Skin*>(skin.get())) {
-            newCase->addWeapon(wpn->clone());
-        }
-    }
-    return newCase;
-}
-
+    
 std::unique_ptr<Item> WeaponCase::selectItem() {
     if (items_.empty()) {
         throw std::runtime_error("The WeaponCase is empty!");
     }
 
-    std::discrete_distribution<int> dist{
-        80,   // Mil-Spec
-        16,    // Restricted
-        3.2,    // Classified
-        0.64,   // Covert
-        0.16    // Rare Special Item
-    };
+    std::uniform_real_distribution<double> dist(0.0, 100.0);
+    double roll = dist(rng_);
 
-    int rarityIndex = dist(rng_);
+    int rarityIndex;
+    if (roll < 80.0) {
+        rarityIndex = 0;
+    } else if (roll < 96.0) {
+        rarityIndex = 1;
+    } else if (roll < 99.2) {
+        rarityIndex = 2;
+    } else if (roll < 99.84) {
+        rarityIndex = 3;
+    } else {
+        rarityIndex = 4;
+    }
 
     std::vector<Weapon_Skin*> candidates;
-
-    for ( auto& skinPtr : items_) {
-        if (auto* wpn = dynamic_cast<Weapon_Skin*>(skinPtr.get())) {
-            std::string rarity = wpn->get_rarity();
+    for (auto& item : items_) {
+        if (auto* wpn = dynamic_cast<Weapon_Skin*>(item.get())) {
             int itemRarity = -1;
-
-            if (rarity == "Mil-Spec") itemRarity = 0;
-            else if (rarity == "Restricted") itemRarity = 1;
-            else if (rarity == "Classified") itemRarity = 2;
-            else if (rarity == "Covert") itemRarity = 3;
-            else if (rarity == "Rare Special Item") itemRarity = 4;
+            std::string r = wpn->get_rarity();
+            if (r == "Mil-Spec") itemRarity = 0;
+            else if (r == "Restricted") itemRarity = 1;
+            else if (r == "Classified") itemRarity = 2;
+            else if (r == "Covert") itemRarity = 3;
+            else if (r == "Rare Special Item") itemRarity = 4;
 
             if (itemRarity == rarityIndex) {
                 candidates.push_back(wpn);
@@ -49,17 +45,17 @@ std::unique_ptr<Item> WeaponCase::selectItem() {
         }
     }
 
-    if (candidates.empty()) {
-        std::uniform_int_distribution<size_t> idxDist(0, items_.size() - 1);
-        return items_[idxDist(rng_)]->clone();
-    }
-
-    std::uniform_int_distribution<size_t> idxDist(0, candidates.size() - 1);
-    return candidates[idxDist(rng_)]->clone();
+    std::uniform_int_distribution<size_t> idx(0, candidates.size() - 1);
+    return candidates[idx(rng_)]->clone();
 }
 
-void WeaponCase::addWeapon(std::unique_ptr<Item> skin) {
+void WeaponCase::addWeapon(std::unique_ptr<Weapon_Skin> skin) {
     if (skin) {
         items_.push_back(std::move(skin));
     }
+}
+
+WeaponCase& WeaponCase::operator +=(std::unique_ptr<Weapon_Skin> skin) {
+    addWeapon(std::move(skin));
+    return *this;
 }
